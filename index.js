73b630bc56bb1e3a4f97510e9186fc4700e98736
Jayeshkhandelwal09 <khandelwaljayesh10@gmail.com>
2023-08-21 18:43:52 +0530

    Backend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { connection } = require("./db");
 const { userRouter } = require("./routes/user.routes");
 const { appointmentRouter } = require("./routes/appointment.routes");
+const cors = require("cors")
 require("dotenv").config()
 
 const app = express();
